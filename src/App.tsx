25965015166d8159d41ad42de73cce4e130f5b68
João Victor <108674777+jrath29@users.noreply.github.com>
2023-12-13 15:45:08 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Generator from './components/Generator'
+
+function App(){
+  return (
+    <div className="App">
+      <h1>Sorteador de Números</h1>
+      <Generator/>
+    </div>
+  );
+}
+
+export default App;
